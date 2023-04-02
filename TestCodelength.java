62d671777fedd2406d86bdc7722fe6f5225fc1d6
bcrow99 <59273209+bcrow99@users.noreply.github.com>
2023-04-02 16:06:15 -0700

    testing

---
+++ TestCodelength.java
@@ -0,0 +1,18 @@
+public class TestCodelength
+{
+	public static void main(String[] args)
+	{
+		TestCodelength tester = new TestCodelength();
+	}
+    
+	public TestCodelength()
+	{
+	    int [] weight = {20, 17, 6, 3, 2, 2, 2, 1, 1, 1};
+	    
+	    int [] length = DeltaMapper.getHuffmanLength(weight);
+	    
+	    System.out.println("Got here.");
+	    for(int i = 0; i < length.length; i++)
+	    	System.out.println(length[i] + " ");
+	}
+}
