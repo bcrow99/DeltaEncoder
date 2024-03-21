42b8c61927b026c0ded32db23ef1a124a73e472c
bcrow99 <59273209+bcrow99@users.noreply.github.com>
2024-03-21 16:29:02 -0700

    cleaned up the code

--- ResizeMapper.java
+++ ResizeMapper.java
@@ -4,43 +4,6 @@ import java.lang.Math.*;
 
 public class ResizeMapper
 {
-	// We'll assume all the image dimensions are even.
-	public static int [] shrinkX(int [] src, int xdim)
-	{
-	    int ydim          = src.length / xdim;
-	    
-	    int shrunken_xdim = xdim / 2;
-	   
-	    int [] dst    = new int[ydim * shrunken_xdim];
-	    
-	    for(int i = 0; i < ydim; i++)
-	        for(int j = 0; j < xdim; j += 2)
-	    		   dst[i * shrunken_xdim + j / 2] = (src[i * xdim + j] + src[i * xdim + j + 1]) / 2;
-	    return dst;
-	}
-	
-	public static int [] shrinkY(int [] src, int xdim)
-	{
-		int ydim          = src.length / xdim;
-		int shrunken_ydim = ydim / 2;
-		
-		int [] dst    = new int[shrunken_ydim * xdim];
-	     
-	    for(int j = 0; j < xdim; j++)
-	    	for(int i = 0; i < ydim; i += 2)  
-	    		dst[i / 2 * xdim + j] = (src[i * xdim + j] + src[(i + 1) * xdim + j]);
-	    return dst;
-	}
-
-	public static int [] shrink(int [] src, int xdim)
-	{
-		int ydim = src.length / xdim;
-		int shrunken_xdim = xdim / 2;
-		int [] temp = shrinkX(src, xdim);
-		int [] dst  = shrinkY(temp, shrunken_xdim);
-		return dst;
-	}
-	
 	// These functions accept arbitrary dimensions.
 	public static int [] resizeX(int src[], int xdim, int new_xdim)
 	{
@@ -319,73 +282,5 @@ public class ResizeMapper
 		int [] tmp = resizeX(src, xdim, new_xdim);
 		int [] dst = resizeY(tmp, new_xdim, new_ydim);
 		return dst;
-	}
-	
-	public static int [] expandX(int src[], int xdim)
-	{
-		int ydim = src.length / xdim;
-		int new_xdim = xdim * 2;
-	
-		int [] dst = new int[new_xdim * ydim];
-		for(int i = 0; i < ydim; i++)
-		{
-			for(int j = 0; j < xdim; j++)
-			{
-				dst[i * new_xdim + 2 * j]     = src[i * xdim + j];
-				dst[i * new_xdim + 2 * j + 1] = src[i * xdim + j];
-			}
-		}
-		return dst;
-	}
-	
-	public static int [] expandY(int [] src, int xdim)
-	{
-		int ydim     = src.length / xdim;
-		int new_ydim = 2 * ydim;
-		
-		int [] dst = new int[xdim * new_ydim];
-		for(int i = 0; i < ydim; i++)
-		{
-			for(int j = 0; j < xdim; j++)
-			{  
-				dst[2 * i * xdim + j] = src[i * xdim + j];  
-		        dst[2 * i * xdim + xdim + j] = src[i * xdim + j];
-			}
-		}
-	    return dst;	
-	}
-	
-	public static int [] expand(int [] src, int xdim)
-	{
-		int [] tmp = expandX(src, xdim);
-		int [] dst = expandY(tmp, xdim * 2);
-		return dst;
-	}
-	
-
-    public static int [] extract(int [] src, int xdim, int xoffset, int yoffset, int new_xdim, int new_ydim)
-    {
-    	int [] dst = new int[new_xdim * new_ydim];
-    	
-    	for(int i = 0; i < new_ydim; i++)
-    	{
-    	    for(int j = 0; j < new_xdim; j++)
-    	    {
-    	    	dst[i * new_xdim + j] = src[(i + yoffset) * xdim + (j + xoffset)];
-    	    }
-    	}
-    	return dst;
-    }
-    	   
-	public static void insert(int [] src, int src_xdim, int [] dst, int dst_xdim, int xoffset, int yoffset, int xdim, int ydim)
-	{
-	    for(int i = 0; i < ydim; i++)
-	    {
-	    	for(int j = 0; j < xdim; j++)
-	    	{
-	    	    dst[i * dst_xdim + j] = src[(i + yoffset) * src_xdim + (j + xoffset)];
-	    	    dst[(i + yoffset) * dst_xdim + (j + xoffset)] = src[i * src_xdim + j];
-	    	}
-	    }
 	}
 }
