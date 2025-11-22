f794fed819bf6070aa1b00c8a83f90670acbad2a
bcrow99 <59273209+bcrow99@users.noreply.github.com>
2025-11-21 19:59:34 -0800

    testing range

--- TestRange.java
+++ TestRange.java
@@ -12,7 +12,9 @@ public class TestRange
 	
 	public TestRange()
 	{
-	    byte [] message = new byte[10];
+	    
+		
+		/*byte [] message = new byte[10];
 	    
 	    message[0] = 4;
 	    message[1] = 1;
@@ -24,7 +26,17 @@ public class TestRange
 	    message[7] = 1;
 	    message[8] = 1;
 	    message[9] = 127;
+	    */
 	   
+		byte [] message = new byte [640 * 2];
+		
+		for(int i = 0; i < 640; i++)
+		{
+			for(int j = 0; j < 2; j++)
+			{
+			    message[i] = (byte)(i * j % 256);
+			}
+		}
 		
 	    boolean [] isSymbol = new boolean[256];
 	    int     [] freq     = new int[256];
@@ -65,6 +77,8 @@ public class TestRange
 	 
 	   
 	    BigInteger [] value = CodeMapper.getRangeQuotient(message, symbol_table, f, sum);
+	    System.out.println("Got here.");
+	    /*
 	    BigDecimal location = new BigDecimal(value[0]);
 	    BigDecimal divisor  = new BigDecimal(value[1]);
 	    location            = location.divide(divisor);
@@ -76,7 +90,7 @@ public class TestRange
 	    for(int i = 0; i < decoded_message.length; i++)
 	    	    System.out.print(decoded_message[i] + " ");
 	    System.out.println();
-	    
+	    */
 	   
 	}
 }
