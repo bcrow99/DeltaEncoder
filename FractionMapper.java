8f367c5abe0d06cdddad47d294d22e5a4b3bdc19
bcrow <bcrow@bcrow-Inspiron-7506-2n1>
2026-09-03 21:54:07 -0700

    added version number

--- FractionMapper.java
+++ FractionMapper.java
@@ -2,6 +2,8 @@ import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.ArrayList;
 
+// verson 1.0
+
 public class FractionMapper
 {
 	public static boolean isProbablePrime(int n)
